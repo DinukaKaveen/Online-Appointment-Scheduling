5ea18785c2465b153c9d0231ebbfa2ed00219df0
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-08-09 13:51:03 +0530

    Update SeekerController.java

--- back-end/src/main/java/com/example/backend/controller/SeekerController.java
+++ back-end/src/main/java/com/example/backend/controller/SeekerController.java
@@ -1,4 +1,20 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.Seeker;
+import com.example.backend.repository.SeekerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SeekerController {
+
+    @Autowired
+    private SeekerRepository SeekerRepo;
+
+    @PostMapping("/RegisterSeeker")
+    Seeker newSeeker(@RequestBody Seeker newSeeker){
+        return SeekerRepo.save(newSeeker);
+    }
 }
