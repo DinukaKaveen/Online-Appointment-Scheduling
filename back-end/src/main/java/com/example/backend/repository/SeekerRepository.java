accad2a4174418f455d5f316036f4a1a165a6bd8
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-08-09 13:28:07 +0530

    Update SeekerRepository.java

--- back-end/src/main/java/com/example/backend/repository/SeekerRepository.java
+++ back-end/src/main/java/com/example/backend/repository/SeekerRepository.java
@@ -1,5 +1,8 @@
 package com.example.backend.repository;
 
-public interface SeekerRepository {
-    
+import com.example.backend.model.Seeker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeekerRepository extends JpaRepository <Seeker,Integer> {
+
 }
