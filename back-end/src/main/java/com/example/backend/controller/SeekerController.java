4b150abb44d4141b2f27c9bddfe0750311cd49bb
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-08-10 01:01:08 +0530

    seeker registration

--- back-end/src/main/java/com/example/backend/controller/SeekerController.java
+++ back-end/src/main/java/com/example/backend/controller/SeekerController.java
@@ -3,18 +3,22 @@ package com.example.backend.controller;
 import com.example.backend.model.Seeker;
 import com.example.backend.repository.SeekerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://127.0.0.1:3000")
 public class SeekerController {
 
     @Autowired
     private SeekerRepository SeekerRepo;
 
     @PostMapping("/RegisterSeeker")
-    Seeker newSeeker(@RequestBody Seeker newSeeker){
-        return SeekerRepo.save(newSeeker);
+    public ResponseEntity<Seeker> newSeeker(@RequestBody Seeker newSeeker){
+        SeekerRepo.save(newSeeker);
+        return ResponseEntity.ok(newSeeker);
     }
 }
