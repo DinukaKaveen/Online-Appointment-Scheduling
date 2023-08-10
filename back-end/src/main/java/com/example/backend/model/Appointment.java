6c7e1b5672afe4cd7898282804cd6a9a48820ffd
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-08-10 23:00:51 +0530

    Update Appointment.java

--- back-end/src/main/java/com/example/backend/model/Appointment.java
+++ back-end/src/main/java/com/example/backend/model/Appointment.java
@@ -1,4 +1,77 @@
 package com.example.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
 public class Appointment {
+
+    @Id
+    @GeneratedValue
+    private Integer appointment_id;
+    private String date;
+    private String time;
+    private String category;
+    private String consultant_name;
+    private String message;
+    private String status;
+
+    public Integer getAppointment_id() {
+        return appointment_id;
+    }
+
+    public void setAppointment_id(Integer appointment_id) {
+        this.appointment_id = appointment_id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getConsultant_name() {
+        return consultant_name;
+    }
+
+    public void setConsultant_name(String consultant_name) {
+        this.consultant_name = consultant_name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
