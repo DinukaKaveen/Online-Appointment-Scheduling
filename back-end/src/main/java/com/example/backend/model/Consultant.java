ff4092c5751d81fc17a7326753bc7635f73f5191
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-08-11 17:59:29 +0530

    Update Consultant.java

--- back-end/src/main/java/com/example/backend/model/Consultant.java
+++ back-end/src/main/java/com/example/backend/model/Consultant.java
@@ -1,4 +1,75 @@
 package com.example.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Consultant {
+
+    @Id
+    @GeneratedValue
+    private Integer consultantId;
+    private String consultantFirstName;
+    private String consultantLastName;
+    private String category;
+    private String phoneNo;
+    private String email;
+    private String address;
+
+    public Integer getConsultantId() {
+        return consultantId;
+    }
+
+    public void setConsultantId(Integer consultantId) {
+        this.consultantId = consultantId;
+    }
+
+    public String getConsultantFirstName() {
+        return consultantFirstName;
+    }
+
+    public void setConsultantFirstName(String consultantFirstName) {
+        this.consultantFirstName = consultantFirstName;
+    }
+
+    public String getConsultantLastName() {
+        return consultantLastName;
+    }
+
+    public void setConsultantLastName(String consultantLastName) {
+        this.consultantLastName = consultantLastName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
