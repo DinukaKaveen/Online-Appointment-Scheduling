444110d27946d2ecf0020737ecb68cf67e790772
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-08-11 23:01:15 +0530

    Update AvailabilityController.java

--- back-end/src/main/java/com/example/backend/controller/AvailabilityController.java
+++ back-end/src/main/java/com/example/backend/controller/AvailabilityController.java
@@ -4,10 +4,9 @@ import com.example.backend.model.Availability;
 import com.example.backend.repository.AvailabilityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://127.0.0.1:3000")
@@ -20,5 +19,10 @@ public class AvailabilityController {
     public ResponseEntity<Availability> newAvailability(@RequestBody Availability newAvailability){
         availabilityRepo.save(newAvailability);
         return ResponseEntity.ok(newAvailability);
+    }
+
+    @GetMapping("/Availability")
+    List<Availability> getAllAvailability(){
+        return availabilityRepo.findAll();
     }
 }
