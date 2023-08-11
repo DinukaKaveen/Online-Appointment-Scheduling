1727680bf50e7b7884b1bf85ad598bd1264008da
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-08-11 18:20:37 +0530

    Update ConsultantController.java

--- back-end/src/main/java/com/example/backend/controller/ConsultantController.java
+++ back-end/src/main/java/com/example/backend/controller/ConsultantController.java
@@ -1,4 +1,22 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.Consultant;
+import com.example.backend.repository.ConsultantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://127.0.0.1:3000")
 public class ConsultantController {
+
+    @Autowired
+    private ConsultantRepository consultantRepo;
+
+    @GetMapping("/Consultants/{consultantId}")
+    public Consultant getConsultantById(@PathVariable Integer consultantId){
+        return consultantRepo.findById(consultantId);
+    }
 }
