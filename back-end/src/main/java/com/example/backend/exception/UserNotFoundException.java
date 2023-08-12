da669fbf79b83271618f65feafc7213ba7b64e04
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-08-12 12:24:43 +0530

    Update UserNotFoundException.java

--- back-end/src/main/java/com/example/backend/exception/UserNotFoundException.java
+++ back-end/src/main/java/com/example/backend/exception/UserNotFoundException.java
@@ -2,7 +2,7 @@ package com.example.backend.exception;
 
 public class UserNotFoundException extends RuntimeException{
 
-    public UserNotFoundException(Integer consultantId){
-        super("Could not found the user with id "+ consultantId);
+    public UserNotFoundException(Integer id){
+        super("Could not found the user with id "+ id);
     }
 }
