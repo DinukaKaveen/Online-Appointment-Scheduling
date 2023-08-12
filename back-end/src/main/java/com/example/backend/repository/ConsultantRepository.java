9b6add9fc4ab47957bf453fa3610c8decf33cac5
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-08-12 12:04:46 +0530

    Update ConsultantRepository.java

--- back-end/src/main/java/com/example/backend/repository/ConsultantRepository.java
+++ back-end/src/main/java/com/example/backend/repository/ConsultantRepository.java
@@ -4,4 +4,5 @@ import com.example.backend.model.Consultant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConsultantRepository extends JpaRepository <Consultant,Integer> {
+    Consultant findByUsername(String username);
 }
