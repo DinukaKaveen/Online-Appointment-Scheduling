669300acf6c8d6acbd36cd4fc957d801b5ca62e5
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-09-08 00:09:16 +0530

    Update Admin.java

--- back-end/src/main/java/com/example/backend/model/Admin.java
+++ back-end/src/main/java/com/example/backend/model/Admin.java
@@ -1,4 +1,83 @@
 package com.example.backend.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+
 public class Admin {
+    @Id
+    @GeneratedValue
+
+    private Integer AdminId;
+    private String AdminFirstName;
+    private String AdminLastName;
+    private String AdminType;
+    private String AdminEmail;
+    private String AdminPhoneNo;
+    private String AdminUsername;
+    private String AdminPassword;
+
+    public Integer getAdminId() {
+        return AdminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        AdminId = adminId;
+    }
+
+    public String getAdminFirstName() {
+        return AdminFirstName;
+    }
+
+    public void setAdminFirstName(String adminFirstName) {
+        AdminFirstName = adminFirstName;
+    }
+
+    public String getAdminLastName() {
+        return AdminLastName;
+    }
+
+    public void setAdminLastName(String adminLastName) {
+        AdminLastName = adminLastName;
+    }
+
+    public String getAdminType() {
+        return AdminType;
+    }
+
+    public void setAdminType(String adminType) {
+        AdminType = adminType;
+    }
+
+    public String getAdminEmail() {
+        return AdminEmail;
+    }
+
+    public void setAdminEmail(String adminEmail) {
+        AdminEmail = adminEmail;
+    }
+
+    public String getAdminPhoneNo() {
+        return AdminPhoneNo;
+    }
+
+    public void setAdminPhoneNo(String adminPhoneNo) {
+        AdminPhoneNo = adminPhoneNo;
+    }
+
+    public String getAdminUsername() {
+        return AdminUsername;
+    }
+
+    public void setAdminUsername(String adminUsername) {
+        AdminUsername = adminUsername;
+    }
+
+    public String getAdminPassword() {
+        return AdminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        AdminPassword = adminPassword;
+    }
 }
