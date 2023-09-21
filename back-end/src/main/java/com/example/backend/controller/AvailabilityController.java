9e962797a1d00e0b94b0125f9543ecacd575d39b
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-09-22 01:00:45 +0530

    create edit availability

--- back-end/src/main/java/com/example/backend/controller/AvailabilityController.java
+++ back-end/src/main/java/com/example/backend/controller/AvailabilityController.java
@@ -1,5 +1,6 @@
 package com.example.backend.controller;
 
+import com.example.backend.exception.UserNotFoundException;
 import com.example.backend.model.Availability;
 import com.example.backend.repository.AvailabilityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,18 @@ public class AvailabilityController {
     public ResponseEntity<Availability> newAvailability(@RequestBody Availability newAvailability){
         availabilityRepo.save(newAvailability);
         return ResponseEntity.ok(newAvailability);
+    }
+
+    @PutMapping("/EditAvailability/{availabilityId}")
+    Availability editAvailability(@RequestBody Availability editAvailability, @PathVariable Integer availabilityId){
+        return availabilityRepo.findById(availabilityId)
+                .map(availability -> {
+                    availability.setDate(editAvailability.getDate());
+                    availability.setTime(editAvailability.getTime());
+                    availability.setAvailability(editAvailability.getAvailability());
+
+                    return availabilityRepo.save(availability);
+                }).orElseThrow(()-> new UserNotFoundException(availabilityId));
     }
 
     @GetMapping("/Availability")
